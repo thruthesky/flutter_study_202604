cd7adaa68534d28dbd6aa9953dbf3b5b8bde7801
JaeHo Song <thruthesky@gmail.com>
2026-04-01 17:17:02 +0800

    refactor main.dart to implement StatefulWidget for counter functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,51 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp($());
+  runApp(A());
 }
 
-class $ extends StatelessWidget {
+class A extends StatefulWidget {
+  createState() => AState();
+}
+
+class AState extends State {
+  int count = 0;
   build(_) {
-    return Container();
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            count++;
+            setState(() {});
+          },
+          child: Text('Count: $count', style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    );
   }
 }
 
-/// State management: why do we need it?
-///
-/// knowledge:
-///   State is a value.
-/// principles:
-/// (1) state changes
-/// (2) state used in multiple places
+
+
+
+
+
 
 
 /// How to have state in Flutter widget.
 /// Widget is a setting/configuration of the UI -> it cannot have a state.
 ///   then? -> Create a state class that can hold the states and attach it to the widget.
+/// 
+/// 1. To have a state, it must be a StatefulWidget.
+/// 
+/// 
+/// 
+/// State management: why do we need it?
+///
+/// knowledge:
+///   State is a value.
+/// principles:
+/// (1) value changes
+/// (2) value used in multiple places
+
